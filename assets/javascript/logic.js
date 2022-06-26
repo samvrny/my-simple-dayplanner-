44d25d8467afeaa2c342f4a02d87fccf2fb97da8
samvrny <samvrny@gmail.com>
2022-06-26 13:37:47 -0500

    made minor progress getting items to save

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,16 +1,27 @@
+var tasks = {};
 var textBoxArr = document.getElementsByClassName("description");
 var tasks;
+var textArea;
 console.log(textBoxArr);
 
 var makeTextArea = function() {
     for (i = 0; i < textBoxArr.length; i++) {
         textArea = document.createElement("textarea");
+        textArea.classList.add("toDos")
         textBoxArr[i].appendChild(textArea);
     }
 };
 
+$(".saveBtn").on("click", function() {
+
+
+    tasks = textArea.text;
+    console.log(tasks);
+    saveTasks();
+  });
+
 var saveTasks = function() {
-    localStorage.setItem("tasks", JSON.stringify(tasks))
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
 makeTextArea();
